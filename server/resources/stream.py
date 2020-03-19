34e5f07fd95112bbea1accce2f703961ee04886e
tranminhquan <tranminhquan1201@gmail.com>
2020-03-19 11:29:15 +0700

    Update

--- server/resources/stream.py
+++ server/resources/stream.py
@@ -70,13 +70,13 @@ class Stream(Resource):
                 if self.cropped_frame is not None:
 
                     for i,fr in enumerate(self.cropped_frame):
-                        _, self.label, self.prob = self.predictor.predict(fr, None)
-                        # _, self.label, self.prob, cam = self.predictor.predict_with_heatmap(fr, None)
-                        # cams.append(cam)
+                        # _, self.label, self.prob = self.predictor.predict(fr, None)
+                        _, self.label, self.prob, cam = self.predictor.predict_with_heatmap(fr, None)
+                        cams.append(cam)
                         cv2.putText(tframe, str(self.label) + ': ' + str(self.prob), (10 + 400*i,30), 
                                     font, font_scale, colors[i], line_type)
 
-                # encode_return_code, image_buffer = cv2.imencode('.jpg', tframe)
+                encode_return_code, image_buffer = cv2.imencode('.jpg', tframe)
 
                 # encoded_frame = stack_images(tframe, self.cropped_frame, cams)
                 encode_return_code, image_buffer = cv2.imencode('.jpg', tframe)
